8ecf8d1db395e46017f0a7018798d55a1f736b60
Josh Cook <jcook2303@gmail.com>
2023-02-25 09:57:52 -0800

    JS for creating all the buttons, styling for display and adding placeholder numbers.

--- calculator.js
+++ calculator.js
@@ -15,13 +15,69 @@ function arraySum(numArray) {
   return overallSum;
 };
 
-function multiply(numArray) {
-  let overallSum = numArray.reduce((total, num) => {
-    return total * num;
-  }, 1)
-  return overallSum;
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
 };
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
 
 function power(firstNumber, secondNumber) {
 	return firstNumber ** secondNumber;
 };
+
+function operate(operator, firstNumber, secondNumber) {
+    if (operator === "+") {
+        let newNumber = add(firstNumber, secondNumber);
+        return newNumber;
+    } else if (operator === "-") {
+        let newNumber = subtract(firstNumber, secondNumber);
+        return newNumber;
+    } else if (operator === "*") {
+        let newNumber = multiply(firstNumber, secondNumber);
+        return newNumber;
+    } else if (operator === "/") {
+        let newNumber = divide(firstNumber, secondNumber);
+        return newNumber;
+    } else if (operator === "**") {
+        let newNumber = power(firstNumber, secondNumber);
+        return newNumber;
+    }
+}
+
+// Creating buttons for the calculator pad
+const buttonList = ["7", "8", "9", "x",
+    "4", "5", "6", "/",
+    "1", "2", "3", "+",
+    "0", ".", "-", "=",
+    "hold", "hold", "hold", "hold", 
+]
+const calcPad = document.querySelector(".calc-pad");
+for (let x = 0; x < 20; x++) {
+    const calcButton = document.createElement(`button`);
+    calcButton.classList.add(`button-${x}`);
+    calcButton.style.height = "50px";
+    calcButton.style.width = "60px";
+    calcButton.style.margin = "2.5px";
+    calcButton.style.backgroundColor = "white";
+    calcButton.style.borderColor = "black";
+    calcButton.style.borderStyle = "solid";
+    calcButton.style.borderWidth = "1px";
+    calcButton.style.borderRadius = "5px";
+    calcButton.style.display = "flex";
+    calcButton.style.alignItems = "center";
+    calcButton.textContent = `${buttonList[x]}`;
+    calcButton.style.fontSize = "20px";
+    calcButton.style.justifyContent = "center";
+    calcPad.appendChild(calcButton);
+}
+
+// JS styling for the number display
+const calcDisplay = document.querySelector(".calc-display");
+calcDisplay.style.display = "flex";
+calcDisplay.style.justifyContent = "flex-end";
+calcDisplay.style.alignItems = "center";
+calcDisplay.textContent = "12345678";
+calcDisplay.style.fontSize = "35px";
+calcDisplay.style.paddingRight = "10px";
